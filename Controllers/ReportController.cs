d5054ac8dccb9d79ea2aa761684f1bb47979293b
programmernawaz <nawazpro8@gmail.com>
2025-09-17 02:01:10 +0530

    add file

---
+++ Controllers/ReportController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PathLabAPI.Data;
+using PathLabAPI.Dto;
+
+namespace PathLabAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportController : ControllerBase
+    {
+
+        private readonly PathLabDbContext _context;
+        public ReportController(PathLabDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("TestReports")]
+        public async Task<ActionResult<IEnumerable<ReportDto>>> GetTestReports()
+        {
+            var data = await (from to in _context.TestOrders
+                              join d in _context.Doctors on to.DoctorId equals d.Id
+                              join p in _context.Patients on to.PatientId equals p.Id
+                              join toi in _context.TestOrderItems on to.Id equals toi.TestOrderId
+                              join l in _context.LabTests on toi.LabTestId equals l.Id
+                              select new ReportDto
+                              {
+                                  PatientId = p.Id,
+                                  PatientName = p.Name,
+                                  DoctorName = d.Name,
+                                  TestName = l.Name
+                              }).ToListAsync();
+
+            return Ok(data);
+        }
+    }
+
+}
---
+++ Dto/ReportDto.cs
@@ -0,0 +1,10 @@
+﻿namespace PathLabAPI.Dto
+{
+    public class ReportDto
+    {
+        public int PatientId { get; set; }
+        public string PatientName { get; set; }= string.Empty;          
+        public string DoctorName { get; set; }= string.Empty;
+        public string TestName { get; set; } = string.Empty;
+    }
+}
